b82c8d7d9189b704ed76ffdca62018bc8ef92305
Melodyh20809 <melodyh20809@gmail.com>
2023-06-20 21:40:20 +0800

    change "depth"

--- src/player/alphabeta.cpp
+++ src/player/alphabeta.cpp
@@ -40,17 +40,20 @@ void read_board(std::ifstream& fin) {
  */
 void write_valid_spot(std::ofstream& fout) {
   // Keep updating the output until getting killed.
+  int depth = 1;
   while(true) {
     // Choose a random spot.
-    auto move = Alphabeta::get_move(root, 3);
+    auto move = Alphabeta::get_move(root, depth);
+    
     fout << move.first.first << " " << move.first.second << " "\
          << move.second.first << " " << move.second.second << std::endl;
-    
+    depth += 1;
+   
+}    
     // Remember to flush the output to ensure the last action is written to file.
     fout.flush();
-    break;
-  }
 }
+
 
 
 /**
